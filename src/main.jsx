3cc047e2a63f46e0099aa5fd62eb212c0e40bb7b
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2025-09-09 20:20:01 +0100

    deleting all exting files

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,3 @@
 export default function Main() {
-  return (
-    <header>
-      <img src="/src/assets/global.png" alt="The global logo" />
-      <span>My travel Journal </span>
-    </header>
-  );
+  return <header></header>;
 }
